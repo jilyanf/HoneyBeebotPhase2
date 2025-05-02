4f459647391a3a7f47c7e1767cc48aa2abeeeb9b
Alex <zcfigueroa@up.edu.ph>
2025-05-02 11:28:45 +0800

    delete functionality

--- HoneyOS/HoneyOS/Form4.Designer.cs
+++ HoneyOS/HoneyOS/Form4.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.delete = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -40,11 +41,22 @@
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(538, 63);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 1;
+            this.delete.Text = "delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.delete);
             this.Controls.Add(this.listBox1);
             this.Name = "Form4";
             this.Text = "Form4";
@@ -55,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button delete;
     }
 }
--- HoneyOS/HoneyOS/Form4.cs
+++ HoneyOS/HoneyOS/Form4.cs
@@ -46,5 +46,41 @@ namespace HoneyOS
         {
 
         }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select an item to delete permanently.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string selectedItem = listBox1.SelectedItem.ToString();
+            string recycleBinPath = Path.Combine(Application.StartupPath, "RecycleBin");
+            string fullPath = Path.Combine(recycleBinPath, selectedItem);
+
+            try
+            {
+                // Check if it's a file or a directory and delete accordingly
+                if (File.Exists(fullPath))
+                {
+                    File.Delete(fullPath);
+                }
+                else if (Directory.Exists(fullPath))
+                {
+                    Directory.Delete(fullPath, true); // true to delete contents
+                }
+
+                MessageBox.Show("Item permanently deleted.", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Refresh list
+                LoadRecycleBinItems();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting item: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
