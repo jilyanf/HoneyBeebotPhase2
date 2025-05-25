b8766eab74d54eadfce4c715b2098f0401f5a8c0
gelo <angelotorres200425@gmail.com>
2025-05-25 15:01:08 +0800

    Honeybee theme for Forms 9, 11, 12

--- HoneyOS/HoneyOS/Form12.Designer.cs
+++ HoneyOS/HoneyOS/Form12.Designer.cs
@@ -45,39 +45,46 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(107, 95);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(134)))), ((int)(((byte)(11)))));
+            this.label1.Location = new System.Drawing.Point(150, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.Size = new System.Drawing.Size(232, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Allocation Strategy";
-            //this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(98, 203);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(134)))), ((int)(((byte)(11)))));
+            this.label2.Location = new System.Drawing.Point(111, 203);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(146, 16);
+            this.label2.Size = new System.Drawing.Size(285, 32);
             this.label2.TabIndex = 1;
             this.label2.Text = "Defragmentation Policy";
-            //this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(85, 315);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(134)))), ((int)(((byte)(11)))));
+            this.label3.Location = new System.Drawing.Point(94, 293);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 16);
+            this.label3.Size = new System.Drawing.Size(311, 32);
             this.label3.TabIndex = 2;
             this.label3.Text = "Defragmentation Strategy";
-            //this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.checkBox1.Location = new System.Drawing.Point(430, 65);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(68, 20);
+            this.checkBox1.Size = new System.Drawing.Size(81, 27);
             this.checkBox1.TabIndex = 3;
             this.checkBox1.Text = "FirstFit";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -85,9 +92,11 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.checkBox2.Location = new System.Drawing.Point(430, 104);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(70, 20);
+            this.checkBox2.Size = new System.Drawing.Size(82, 27);
             this.checkBox2.TabIndex = 4;
             this.checkBox2.Text = "BestFit";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -95,9 +104,11 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.checkBox3.Location = new System.Drawing.Point(430, 141);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(78, 20);
+            this.checkBox3.Size = new System.Drawing.Size(94, 27);
             this.checkBox3.TabIndex = 5;
             this.checkBox3.Text = "WorstFit";
             this.checkBox3.UseVisualStyleBackColor = true;
@@ -105,9 +116,11 @@
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.checkBox4.Location = new System.Drawing.Point(430, 203);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(98, 20);
+            this.checkBox4.Size = new System.Drawing.Size(120, 27);
             this.checkBox4.TabIndex = 6;
             this.checkBox4.Text = "OnDemand";
             this.checkBox4.UseVisualStyleBackColor = true;
@@ -115,9 +128,11 @@
             // checkBox5
             // 
             this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.checkBox5.Location = new System.Drawing.Point(430, 230);
             this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(79, 20);
+            this.checkBox5.Size = new System.Drawing.Size(92, 27);
             this.checkBox5.TabIndex = 7;
             this.checkBox5.Text = "Periodic";
             this.checkBox5.UseVisualStyleBackColor = true;
@@ -125,9 +140,11 @@
             // checkBox6
             // 
             this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.checkBox6.Location = new System.Drawing.Point(430, 257);
             this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(66, 20);
+            this.checkBox6.Size = new System.Drawing.Size(77, 27);
             this.checkBox6.TabIndex = 8;
             this.checkBox6.Text = "Never";
             this.checkBox6.UseVisualStyleBackColor = true;
@@ -135,9 +152,11 @@
             // checkBox7
             // 
             this.checkBox7.AutoSize = true;
+            this.checkBox7.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.checkBox7.Location = new System.Drawing.Point(430, 298);
             this.checkBox7.Name = "checkBox7";
-            this.checkBox7.Size = new System.Drawing.Size(110, 20);
+            this.checkBox7.Size = new System.Drawing.Size(132, 27);
             this.checkBox7.TabIndex = 9;
             this.checkBox7.Text = "SimpleMerge";
             this.checkBox7.UseVisualStyleBackColor = true;
@@ -145,26 +164,35 @@
             // checkBox8
             // 
             this.checkBox8.AutoSize = true;
+            this.checkBox8.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.checkBox8.Location = new System.Drawing.Point(430, 325);
             this.checkBox8.Name = "checkBox8";
-            this.checkBox8.Size = new System.Drawing.Size(151, 20);
+            this.checkBox8.Size = new System.Drawing.Size(179, 27);
             this.checkBox8.TabIndex = 10;
             this.checkBox8.Text = "MobeBlocksTOStart";
             this.checkBox8.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(215)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(165)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.button1.Location = new System.Drawing.Point(304, 372);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 45);
             this.button1.TabIndex = 11;
             this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form12
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(205)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox8);
@@ -179,7 +207,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form12";
-            this.Text = "Form12";
+            this.Text = "HoneyOS Defragmentation Options";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- HoneyOS/HoneyOS/Form12.cs
+++ HoneyOS/HoneyOS/Form12.cs
@@ -53,5 +53,15 @@ namespace HoneyOS
         private void label1_Click(object sender, EventArgs e) { }
         private void label2_Click(object sender, EventArgs e) { }
         private void label3_Click(object sender, EventArgs e) { }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
